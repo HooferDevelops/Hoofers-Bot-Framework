da5808e51137712d058beb280e83764166bf1309
HooferDevelops <60201971+HooferDevelops@users.noreply.github.com>
2020-05-11 22:24:51 -0400

    Setup Basic File Structure

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 const Discord = require("discord.js");
-const Client = new discord.Client();
+const client = new Discord.Client();
+const fs = require("fs");
 
-Client.on("ready",()=>{
+global.validateCommand = require("./modules/validateCommand");
+global.loadCommands = require("./modules/loadCommands");
+global.config = JSON.parse(fs.readFileSync("./config.json"))
 
+client.on("ready",()=>{
+    loadCommands();
+    console.log("Bot Framework has successfully initialized and connected to Discord's Services.");
 });
 
-Client.on("message",()=>{
-    
+client.on("message",(message)=>{
+    // Command Handler
+    validateCommand(message);
 });
 
-Client.login("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("NzA0NTI0MzEyOTA3ODc0MzE0.XroFHw.caNGIEHQ5BepVgACvB-s0MACibA");
